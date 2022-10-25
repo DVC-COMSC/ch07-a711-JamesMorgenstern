f153b36b7fdcadd6cb90c2e46d02ceb102d0239c
JamesMorgenstern <jamesklm@comcast.net>
2022-10-24 21:16:46 -0700

    committed1

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ x = np.arange(2)
 width = 0.2
 labels = ['Math', 'English', 'Physics', 'Computer']
 names = ['Bill', 'Mary']
-
-# ******************************
-# Make your code
-# ******************************
-
+fig, ax = plt.subplots()
+m1 = ax.bar(x-width*1.5, Math, width)
+e1 = ax.bar(x-width*0.5, English, width)
+p1 = ax.bar(x+width*0.5, Physics, width)
+c1 = ax.bar(x+width*1.5, Computer, width)
+ax.legend(labels)
+ax.set_title('Grouped graph for scores')
+ax.bar_label(m1, padding=1)
+ax.bar_label(e1, padding=1)
+ax.bar_label(p1, padding=1)
+ax.bar_label(c1, padding=1)
+ax.set_xticks(x, names)
 
 fig.savefig('A11.png')
